0ce332201266c3360aa7e73a3e01a1fcc576566b
Tyrrance <tyrrancem37@gmail.com>
2023-03-20 09:38:14 -0400

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,40 @@
+
+
+
+
+$(function hour() {
+  var today = dayjs();
+  $('#currentDay').text(today.format('MMM D, YYYY'));
+
+  $('.saveBtn').on('click', function() {
+    var hour = $(this).parent().attr('id');
+    var description =$(this).siblings('.description').val();
+    localStorage.setItem(hour, description);
+
+  });
+
+  $('.time-block').each(function(){
+    var currentHour = dayjs().hour();
+    var hour = parseInt($(this).attr('id').replace('hour-', ''));
+
+
+    
+    if(hour < currentHour){
+      $(this).addClass('past');
+    }else if(hour === currentHour){
+      $(this).addClass('present');
+    }else{
+      $(this).addClass('future');
+    }
+  });
+  $('.time-block').each(function(){
+    var hour = $(this).attr('id');
+    var description = localStorage.getItem(hour);
+
+    if(description) {
+      $(this).children('.description').val(description);
+
+    }
+  });
+});
+
